bbca3ddc1178cf26b1ef22513ace4d5eec50b6b2
vibhor <vibhorpandhare@gmail.com>
2016-09-18 01:04:33 +0530

    a little further than series

--- iAsset/src/reliabilityCalculation/RelCalcLogic.java
+++ iAsset/src/reliabilityCalculation/RelCalcLogic.java
@@ -7,76 +7,90 @@ public class RelCalcLogic {
 
 	Set<Integer> toBeFilledSet = new HashSet<Integer>();
 	Set<Integer> sameLevelSet = new HashSet<Integer>();
-	Set<Integer> probabaleParallelSet = new HashSet<Integer>();
 	Set<Integer> focusParallelSet = new HashSet<Integer>();
-	Set<Integer> probabaleSeriesSet = new HashSet<Integer>();
 	Set<Integer> focusSeriesSet = new HashSet<Integer>();
+	Set<Integer> DNDSet = new HashSet<Integer>();
 
 	SysLvlRelCalc SLRCObject = new SysLvlRelCalc();
 
 	public void Logic(StringToArray Object){
-		int index=1;
-		while(index<=Object.NoOfRows){
-			if(Object.intIsLMA[index-1]==0 && Object.intPAdd[index-1][Object.maxLevelCount-1]!=0){
-				toBeFilledSet.add(index);
-			}
-			index++;
-		}
 
-		int index1=1;
-		while(index1<=Object.NoOfRows){
-			if(Object.intPAdd[index1-1][Object.maxLevelCount-1]!=0&& toBeFilledSet.contains(index1)==false){
-				sameLevelSet.add(index1);
-			}
-			index1++;
-		}
-
-		System.out.println("Set Thory: "+toBeFilledSet+" | "+sameLevelSet+" | "+focusSeriesSet);
-		for(int l=0; l<10; l++){
-			System.out.println((l+1)+" | "+Object.intIsLMA[l]+" | "+Object.intrRel[l]);
-		}
+		int levelIterator = Object.maxLevelCount;
 
 
-
-
-		for(int i: toBeFilledSet){
-			int a=0, b=0;
-			for(int j: sameLevelSet){
-				for(int k = 0; k < Object.maxLevelCount; k++){
-
-					if(Object.intSAdd[i-1][k] != 0 && Object.intSAdd[j-1][k] != Object.intSAdd[i-1][k]) a=1;
-					if(Object.intPAdd[i-1][k] != 0 && Object.intPAdd[j-1][k] != Object.intPAdd[i-1][k]) b=1;
-
+		while(levelIterator>0){
+			int index=2;
+			while(index<=Object.NoOfRows){
+				if(Object.intIsLMA[index-1]==0 && Object.intPAdd[index-1][levelIterator-1]!=0){
+					toBeFilledSet.add(index);
 				}
-				if(a==0 && b==0){
-					focusSeriesSet.add(j);
-				}a=0; b=0;
+				index++;
 			}
 
-			double[] relsInFocus = new double[focusSeriesSet.size()];
-			int count=0;
-
-			for(int v: focusSeriesSet){
-				relsInFocus[count]= Object.intrRel[v-1];
-				count++;
+			int index1=2;
+			while(index1<=Object.NoOfRows){
+				if(Object.intPAdd[index1-1][levelIterator-1]!=0&& toBeFilledSet.contains(index1)==false && DNDSet.contains(index1)==false){
+					sameLevelSet.add(index1);
+				}
+				index1++;
 			}
 
-			Object.intrRel[i-1] = SLRCObject.calcInSeries(relsInFocus);
-			Object.intIsLMA[i-1]=1;
-
-			System.out.println("Set Thory: "+toBeFilledSet+" | "+sameLevelSet+" | "+focusSeriesSet);
+			System.out.println("Set Thory1: "+toBeFilledSet+" | "+sameLevelSet+" | "+focusSeriesSet);
 			for(int l=0; l<10; l++){
 				System.out.println((l+1)+" | "+Object.intIsLMA[l]+" | "+Object.intrRel[l]);
-				
-			focusSeriesSet.removeAll(focusSeriesSet);
-			
-			
 			}
+
+
+			
+			for(int i: toBeFilledSet){
+				int a=0, b=0;
+
+				for(int j: sameLevelSet){
+
+					for(int k = 0; k < levelIterator; k++){
+
+						if(Object.intSAdd[i-1][k] != 0 && Object.intSAdd[j-1][k] != Object.intSAdd[i-1][k]) a=1;
+						if(Object.intPAdd[i-1][k] != 0 && Object.intPAdd[j-1][k] != Object.intPAdd[i-1][k]) b=1;
+
+					}
+					if(a==0 && b==0){
+						focusSeriesSet.add(j);
+						DNDSet.add(j);
+					}a=0; b=0;
+				}
+
+				double[] relsInFocus = new double[focusSeriesSet.size()];
+				int count=0;
+
+				for(int v: focusSeriesSet){
+					relsInFocus[count]= Object.intrRel[v-1];
+					count++;
+				}
+
+				Object.intrRel[i-1] = SLRCObject.calcInSeries(relsInFocus);
+				Object.intIsLMA[i-1]=1;
+
+				System.out.println("Set Thory Series: "+toBeFilledSet+" | "+sameLevelSet+" | "+focusSeriesSet+" | "+focusParallelSet);
+				for(int l=0; l<10; l++){
+					System.out.println((l+1)+" | "+Object.intIsLMA[l]+" | "+Object.intrRel[l]);
+					}
+
+				focusSeriesSet.removeAll(focusSeriesSet);
+			}
+
+
+			levelIterator--;
+			DNDSet.addAll(toBeFilledSet);
+			toBeFilledSet.removeAll(toBeFilledSet);
+			sameLevelSet.removeAll(sameLevelSet);
+
 		}
 
-
-
-
-
 	}
+
+
+
+
+
+
 }
